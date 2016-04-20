2941b7ac275f3744b68d71e185669abc80a6fe44
Marcus Longmuir <marcus.longmuir@improbable.io>
2016-04-20 10:43:47 +0100

    Added postMessage sending of url to parent window on change

--- public/app/boot.js
+++ public/app/boot.js
@@ -6,5 +6,33 @@
   }).catch(function(err) {
     console.log('Loading app module failed: ', err);
   });
+})();
 
+(function() {
+  'use strict';
+
+  // If this window is within an iframe
+  if(window.parent !== window){
+
+    // Override the window.history object's pushState function
+    (function(history) {
+
+      var host = window.location.protocol+"//"+window.location.host;
+      var pushState = history.pushState;
+      history.pushState = function(state, title, url) {
+        // Check if the url is fully-qualified and if so, remove the host
+        if(url.substring(0,4)==='http'){
+          url = url.substring(host.length);
+        }
+
+        // Send the url to the parent frame - the inspector app
+        window.parent.postMessage({
+          metrics_url: url
+        }, '*');
+
+        // Call the original pushState function
+        return pushState.apply(history, arguments);
+      };
+    })(window.history);
+  }
 })();
